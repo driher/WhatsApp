462ac5274ca5c3db91eb85367d2d816220ee61b9
Aa Kasep <driher@gmail.com>
2026-05-28 00:11:13 +0700

    oke kita coba

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  turbopack: {},
+
+  allowedDevOrigins: [
+    "127.0.0.1",
+    "localhost",
+    "192.168.1.9",
+    "https://asp-realtor-parish-must.trycloudflare.com"
+  ],
 };
 
 module.exports = nextConfig;
